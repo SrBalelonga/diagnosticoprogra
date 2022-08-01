76d43e2dc77cb6e6e7038066725c7087bab633ee
unknown <arielortizblanco9@gmail.com>
2022-08-01 16:00:17 -0600

    this is a test

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PolindromoQadir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word, lurk, charac;
+            int i;
+            Console.WriteLine("Ingrese una palabra:");
+            word = Console.ReadLine();
+            i = word.Length;
+            lurk = "";
+            for (int x = 0; x < i; x++)
+            {
+                charac = word.Substring(x, 1);
+                lurk = lurk + charac;
+            }
+            if (word == lurk)
+            {
+                Console.WriteLine("Es un palindromo");
+            }
+            else
+            {
+                Console.WriteLine("No es un palindromo");
+            }
+
+
+
+
+        }
+    }
+} 
